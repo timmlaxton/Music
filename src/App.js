70f8f90a8df2efd767279c85915fb4b5f75b2c8e
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-14 09:21:29 +0000

    added styling to search and lettering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
+import './App.css';
+import {FormGroup, FormControl, InputGroup,} from 'react-bootstrap';
+import {SearchOutlined} from 'react-icons/'
 
 class App extends Component {
     render() {
         return (
-            <div>
-            <div className="App-title">Music Master from App</div>
-            <div>
-                <input placeholder="search an artisy..."/>
-                <button>select</button>
+            <div className="App">
+                <div className="App-title">Music Master</div>
+            <FormGroup>
+                <InputGroup> 
+                <FormControl
+                type="text"
+                placeholder="Search for an Artist">
+                </FormControl>
+                <button>Search</button>
+                </InputGroup>
+            </FormGroup>
+            <div className="Profile">
+            <div>Artist Picture</div>
+            <div>Artist Name</div>
+            </div>
+            <div className="Gallery">
+                Gallery
             </div>
             </div>
+            
         )
             
     }
